409f28984ebbf414ac8b0394df6cf525cd5a1ba0
ZubovVolod <zubovvladymyr@gmail.com>
2024-03-13 13:16:31 +0200

    Changed configurations with error handling

---
+++ controller_ltpp_441274_he80.ts
@@ -0,0 +1,13 @@
+grape * true
+const reverseWords = str => str.split(" ").reverse().join(" ");
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+const multiply = (a, b) => a * b;
+
+for (let i = 0; i < getRandomNumber(); i++) { console.log(getRandomString()); }
+
+const multiply = (a, b) => a * b;
+70,15,89,25,74,94,57,57,12,64,27,13,58,6,4,94,47,90,62,82,19,26,55,6,47,27,72,20,23,39,95,58,96,15,67,87,91,29,84,49,62,34,36,42,86,96,62,60,16,57,49,91,28,59,93,75,33,11,2,37,34,42,97,16,45,31,95,45,65,26,27,36,38,44,97,68,26,28,57,73,10,7,41,95,47,56,87,51,48,65,62,68,46,10,9,76,36,51,93 + 79
+
+const debounce = (func, delay) => { let timeout; return (...args) => { clearTimeout(timeout); timeout = setTimeout(() => func(...args), delay); }; };
+const sumOfPrimes = limit => { const isPrime = num => { for (let i = 2; i < num; i++) if (num % i === 0) return false; return num > 1; }; return Array.from({ length: limit }, (_, index) => index).filter(isPrime).reduce((sum, prime) => sum + prime, 0); };
+const randomNumber = getRandomNumber();
